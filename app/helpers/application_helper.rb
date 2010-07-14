766c58fc572196804b77e9805f56aa359e1341a2
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2010-07-13 23:10:51 -0700

    Making an actual website.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def javascripts_frameworks
+    returning "" do |html|
+      html << javascript_include_tag('prototype')
+      html << javascript_include_tag('http://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.3/scriptaculous.js?load=effects,dragdrop,controls,builder,slider')
+    end
+  end
+
+  def javascripts
+    javascript_include_tag('application')
+  end
+
+  def stylesheets
+    returning "" do |css|
+      css << stylesheet_link_tag("http://yui.yahooapis.com/2.8.1/build/reset/reset-min.css")
+      css << stylesheet_link_tag('application')
+    end
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var NavHelper = {
+    // Smooth scrolls to the anchor
+    scrollOnClick: function(navLink){
+        var anchor = navLink.href.gsub(/.*#/, '');
+        var targetSelector = 'a[name=' + anchor + ']';
+        console.log(targetSelector)
+        var target = $(document.body.down(targetSelector))
+        console.log(target)
+        Element.observe(navLink, 'click', function(event, targetElement){
+            event.stop();
+            Effect.ScrollTo(targetElement, {
+                offset:-300,
+                duration:0.5
+            });
+        }.bindAsEventListener(this, target));
+        
+    }
+}
+// Bind the nav links with smooth scrolling action
+$$('#page_nav li a').each(function(navLink){
+    NavHelper.scrollOnClick(navLink)
+})
